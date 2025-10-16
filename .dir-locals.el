39ab45416597b14c3ca7c064db3ab649e43d0de2
Pablo Stafforini <pablo@stafforini.com>
2025-10-16 13:10:59 -0300

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+((markdown-mode . ((jinx-languages . "pl")
+		   (flycheck-languagetool-language . "pl")
+		   (jinx-local-words . "title bibKey"))))
